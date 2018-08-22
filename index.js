3d56f7c5887c1c2b54030c787904ceb2f00e6b55
nvtuongcs <nvtuongcs@gmail.com>
2018-08-22 08:18:21 +0700

    Add passport

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').config()
 const express = require('express')
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
 const app = express()
 const PORT = process.evn.PORT || 5000
 
-app.get('/', (req, res) => {
-  res.send('Hello World')
-})
+passport.use(new GoogleStrategy())
 
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
