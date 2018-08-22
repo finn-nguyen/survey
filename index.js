c831e11e519fb20790ccf58664db39fed9d36337
nvtuongcs <nvtuongcs@gmail.com>
2018-08-22 18:06:32 +0700

    Add mongoose

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const app = express()
+const mongoose = require('mongoose')
+const keys = require('./config/keys')
 
 require('dotenv').config()
 require('./services/passport')
 require('./routes/authRoutes')(app)
+
+mongoose.connect(keys.mongoURI)
 
 const PORT = process.env.PORT || 5000
 
