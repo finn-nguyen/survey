a4008b6009e371de385e492235664a3bd938b858
nvtuongcs <nvtuongcs@gmail.com>
2018-08-22 17:32:07 +0700

    Authentication with passport

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const express = require('express')
 const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const app = express()
-const PORT = process.evn.PORT || 5000
+const keys = require('./config/keys')
+const PORT = process.env.PORT || 5000
 
-passport.use(new GoogleStrategy())
+passport.use(new GoogleStrategy({
+  clientID: keys.googleClientID,
+  clientSecret: keys.googleClientSecret,
+  callbackURL: '/auth/google/callback'
+}, (accessToken, refreshToken, profile, done) => {
+  console.log('accessToken: ', accessToken)
+  console.log('refreshToken: ', refreshToken)
+  console.log('profile: ', profile)
+}))
+
+app.get('/auth/google', passport.authenticate('google', {
+  scope: ['profile', 'email']
+}))
+
+app.get('/auth/google/callback', passport.authenticate('google'))
 
 app.listen(PORT, () => console.log(`Server is running at port ${PORT}`))
