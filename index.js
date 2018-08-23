27c8f8a0d5bd3475e6b3c0aeb2d2e76056794ff6
nvtuongcs <nvtuongcs@gmail.com>
2018-08-23 08:34:16 +0700

    Add User model

--- index.js
+++ index.js
@@ -2,12 +2,11 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
 const keys = require('./config/keys')
+mongoose.connect(keys.mongoURI)
 
 require('dotenv').config()
 require('./services/passport')
 require('./routes/authRoutes')(app)
-
-mongoose.connect(keys.mongoURI)
 
 const PORT = process.env.PORT || 5000
 
--- services/passport.js
+++ services/passport.js
@@ -2,12 +2,20 @@ const passport = require('passport')
 const GoogleStrategy = require('passport-google-oauth20').Strategy
 const keys = require('../config/keys')
 
+const User = require('../models/User')
+
 passport.use(new GoogleStrategy({
   clientID: keys.googleClientID,
   clientSecret: keys.googleClientSecret,
   callbackURL: '/auth/google/callback'
 }, (accessToken, refreshToken, profile, done) => {
-  console.log('accessToken: ', accessToken)
-  console.log('refreshToken: ', refreshToken)
-  console.log('profile: ', profile)
+  console.log(profile)
+  User.findOne({ googleId: profile.id })
+    .then(user => {
+      if (user) {
+        done(null, user)
+      } else {
+        new User({ googleId: profile.id}).save().then(user => done(null, user))
+      }
+    })
 }))
