1e3a96225ec76cc05ef3344e4755e7ca4c167d0c
nvtuongcs <nvtuongcs@gmail.com>
2018-09-11 18:54:05 +0700

    Insert data to mongodb

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,3 +1,6 @@
+const _ = require('lodash')
+const Path = require('path-parser').default
+const { URL } = require('url')
 const requireLogin = require('../middlewares/requireLogin')
 const requireCredits = require('../middlewares/requireCredits')
 const Mailer = require('../services/mailer')
@@ -5,7 +8,7 @@ const surveyTemplate = require('../services/emailTemplates')
 const Survey = require('../models/Survey')
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) =>{
+  app.get('/api/surveys/:surveyId/:choice', (req, res) =>{
     res.send('Thanks for voting!')
   })
 
@@ -30,5 +33,33 @@ module.exports = app => {
     } catch(err) {
       res.status(422).send(err)
     }
+  })
+
+  app.post('/api/surveys/webhooks', (req, res) => {
+    const events = _.map(req.body, event => {
+      const pathname = new URL(event.url).pathname
+      const p = new Path('/api/surveys/:surveyId/:choice')
+      const match = p.test(pathname)
+      if (match) {
+        return { email: event.email, surveyId: match.surveyId, choice: match.choice }
+      }
+    })
+    const compactEvents = _.compact(events)
+    const uniqueEvents = _.uniqBy(compactEvents, 'email', 'surveyId')
+    _.each(uniqueEvents, (event) => {
+      Survey.updateOne(
+        {
+          _id: event.surveyId,
+          recipients: {
+            $elemMatch: { email: event.email, responded: false }
+          }
+        }, {
+          $inc: { [event.choice]: 1 },
+          $set: { 'recipients.$.responded': true },
+          lastResponded: new Date()
+        }
+      ).exec()
+    })
+    res.send({})
   })
 }
